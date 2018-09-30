77dc1ea924070279af4ca78c6b9a0ccd087c5c18
ProjetoSIS <39012290+ProjetoSIS@users.noreply.github.com>
2018-09-29 21:34:32 -0300

    Update sobrenos.js

--- js/sobrenos.js
+++ js/sobrenos.js
@@ -8,3 +8,13 @@ $('.header').addClass('fixed');}       //fixed
 else{
 $('.header').removeClass('fixed');}       //scroll
 });
+
+
+$(window).scroll(function(){
+
+if ($(this).scrollTop() > 0){
+$('.header-lista').addClass('fixed');}       //fixed
+
+else{
+$('.header-lista').removeClass('fixed');}       //scroll
+});
